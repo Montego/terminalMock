337485f7f8e2f0fcace162c2919e56836db70c17
Montego_RD <bar_d91@mail.ru>
2019-06-28 19:23:11 +0300

    nothing important

--- src/main/java/com/terminalmock/test/entities/dictionary/AddressCountryRegion.java
+++ src/main/java/com/terminalmock/test/entities/dictionary/AddressCountryRegion.java
@@ -2,16 +2,18 @@ package com.terminalmock.test.entities.dictionary;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Data
-@Table(name = "D_AddressCountryRegion")
+@Table(name = "\"D_AddressCountryRegion\"" )
 public class AddressCountryRegion {
     @Id
+    @Column(name = "\"CountryRegionId\"")
     private String countryRegionId;
-
+    @Column(name = "\"Name\"")
     private String name;
 }
--- src/main/java/com/terminalmock/test/entities/enums/Gender.java
+++ src/main/java/com/terminalmock/test/entities/enums/Gender.java
@@ -1,24 +1,19 @@
 package com.terminalmock.test.entities.enums;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name ="\"BE_Gender\"" )
+@Table(name = "\"BE_Gender\"")
 public class Gender {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "\"Id\"", nullable = false)
     private Integer id;
     @Column(name = "\"Name\"")
     private String name;
-
 
 }
