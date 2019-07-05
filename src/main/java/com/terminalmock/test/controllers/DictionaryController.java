5ebc188fd89316bfaf5426e42a919f9680b2d2e8
Montego_RD <bar_d91@mail.ru>
2019-07-05 15:56:28 +0300

    delete /api

--- src/main/java/com/terminalmock/test/controllers/DictionaryController.java
+++ src/main/java/com/terminalmock/test/controllers/DictionaryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/dictionary")
+@RequestMapping("/dictionary")
 public class DictionaryController {
     private final AcademyYearService academyYearService;
 //    private final AccreditationDocService accreditationDocService;
