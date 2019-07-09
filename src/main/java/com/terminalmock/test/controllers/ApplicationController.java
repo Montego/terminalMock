b33f94a8ad3cfae2a7292780dfb748a3e1ae35ac
Montego_RD <bar_d91@mail.ru>
2019-07-09 21:06:17 +0300

    little fix

--- src/main/java/com/terminalmock/test/controllers/ApplicationController.java
+++ src/main/java/com/terminalmock/test/controllers/ApplicationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("api/profile")
 public class ApplicationController {
 
     @Autowired
