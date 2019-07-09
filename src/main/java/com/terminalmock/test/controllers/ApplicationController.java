08275f77a298a417214563927d93a6ef0a0046f0
Вадим Пилипенко <vpilipenko@vita-soft.ru>
2019-07-09 17:48:20 +0300

    Post routes

--- src/main/java/com/terminalmock/test/controllers/ApplicationController.java
+++ src/main/java/com/terminalmock/test/controllers/ApplicationController.java
@@ -1,13 +1,12 @@
 package com.terminalmock.test.controllers;
 
 import com.terminalmock.test.dto.ApplicationShortDto;
+import com.terminalmock.test.entities.entity.Application;
+import com.terminalmock.test.repositories.entityrepo.ApplicationRepo;
 import com.terminalmock.test.repositories.entityrepo.PersonRepo;
 import com.terminalmock.test.services.dtoServices.ApplicationShortDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +18,25 @@ public class ApplicationController {
     ApplicationShortDtoService applicationShortDtoService;
 
     @Autowired
+    ApplicationRepo applicationRepo;
+
+    @Autowired
     PersonRepo personRepo;
+
+    @GetMapping("/getEmptyApplication")
+    public Application getEmptyApplication() {
+        return new Application(true);
+    }
 
     @GetMapping("/applicationByPerson/{id}")
     public List<ApplicationShortDto> applicationByPerson(@PathVariable("id") Long id) {
-
         return applicationShortDtoService.getPersonApplications(personRepo.findById(id).get());
     }
+
+    @PostMapping("/application")
+    public Application applicationByPerson(@RequestBody Application application) {
+        return applicationRepo.save(application);
+
+    }
+
 }
--- src/main/java/com/terminalmock/test/controllers/PersonController.java
+++ src/main/java/com/terminalmock/test/controllers/PersonController.java
@@ -2,20 +2,24 @@ package com.terminalmock.test.controllers;
 
 import com.terminalmock.test.entities.entity.Person;
 import com.terminalmock.test.repositories.entityrepo.PersonRepo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest")
 public class PersonController {
 
+    @Autowired
     PersonRepo personRepo;
 
     @GetMapping("/getEmptyPerson")
     public Person getEmptyMedCertDPO(){
-
         return new Person(true);
+    }
+
+    @PostMapping("/person")
+    public Person save(@RequestBody Person person){
+        return personRepo.save(person);
     }
 
 }
