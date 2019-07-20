658bbd369a08c999143576dafc3dd349e60f766f
Alexey Kozlov <askozlov@vita-soft.ru>
2019-07-20 14:37:02 +0300

    fix addresses

--- src/main/java/com/terminalmock/test/entities/enums/AddressType.java
+++ src/main/java/com/terminalmock/test/entities/enums/AddressType.java
@@ -10,7 +10,6 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name = "\"BE_AddressType\"")
-@EqualsAndHashCode
 public class AddressType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +17,23 @@ public class AddressType {
     private Integer id;
     @Column(name = "\"Name\"")
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AddressType that = (AddressType) o;
+
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        return getName() != null ? getName().equals(that.getName()) : that.getName() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
 }
