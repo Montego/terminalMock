3b8801c7cbcdeb0037103759e275bfa6421f72d2
Alexey Kozlov <askozlov@vita-soft.ru>
2019-07-22 14:33:21 +0300

    fix axDto table and column names

--- src/main/java/com/terminalmock/test/dto/AxDto.java
+++ src/main/java/com/terminalmock/test/dto/AxDto.java
@@ -4,21 +4,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "\"AxDto\"")
 public class AxDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "\"axDtoId\"")
     private Long axDtoId;
+    @Column(name = "\"profileId\"")
     private Long profileId;
+    @Column(name = "\"contactPersonId\"")
     private String contactPersonId;
+    @Column(name = "\"applicationId\"")
     private String applicationId;
+    @Column(name = "\"agreementId\"")
     private String agreementId;
 }
